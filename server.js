77f27a5df5f384da901a532c693be5ffacb3a2a2
SirraSneha18 <156457190+SirraSneha18@users.noreply.github.com>
2025-03-18 15:06:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/screenshot", async (req, res) => {
   try {
